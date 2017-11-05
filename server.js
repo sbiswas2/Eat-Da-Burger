436e6e90bcae55bd6dc798fe8749e74c737f4a99
Setu Biswas <setubiswas@Setus-MacBook-Pro.local>
2017-11-04 20:04:26 -0500

    changed server.js file

--- server.js
+++ server.js
@@ -2,4 +2,26 @@ var express = require("express");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
 
-//remember app.use(express.static('public'))
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+app.use(methodOverride('_method'));
+
+var exphbs = require('express-handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require('./controllers/burgers_controller.js');
+app.use('/', routes);
+
+app.listen(PORT, function() {
+	console.log("App listening on PORT " + PORT);
+});
