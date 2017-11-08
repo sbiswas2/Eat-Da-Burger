1fc90896031931226424fc7847d980a7f33b326f
Setu Biswas <setubiswas@Setus-MacBook-Pro.local>
2017-11-08 10:54:32 -0600

    changed connection

--- config/connection.js
+++ config/connection.js
@@ -6,17 +6,10 @@ if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection({
 	  host: "localhost",
 	  user: "root",
-	  password: "",
-	  database: "burgers_db"
+	  password: "hacktheplanet",
+	  database: "todoagain_db"
 	});
 };
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
+connection.connect();
 module.exports = connection;
